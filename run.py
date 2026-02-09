bcf5490feb3a4c04345f7bfa68efaaea6d9a5aa6
paraleash99 <nehapani4646@gmail.com>
2026-02-09 16:13:33 +0530

    Added comments in run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from app import create_app
+from app import create_app      #Imports create_app() from app/__init__.py
 
-app = create_app()
+app = create_app()   #This line actually builds your app.
 
-if __name__ == '__main__':
-    print("🚀 Starting Flask server...")
+if __name__ == '__main__':   #checks whether the main file is running if true then python run.py
+    print("🚀 Starting Flask server...")   #Prints a message to the backend console
     #print(f"📍 Gemini service loaded: {GEMINI_LOADED}")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)   #host: allows access from other devices, If this were 127.0.0.1, only your machine could access it.
